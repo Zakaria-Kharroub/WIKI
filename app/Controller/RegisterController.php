87c7b1a74346fd3bb69ea63a7c24c2cdfc7bf125
zakaria <zikokh012@gmail.com>
2024-01-09 12:17:32 +0100

    ajouter register model et la methode registeruser

--- app/Controller/RegisterController.php
+++ app/Controller/RegisterController.php
@@ -8,6 +8,26 @@ class RegisterController {
         include_once '../app/View/register.php';
     }
 
+    public function registeruser(){
+        if (isset($_POST['submit'])){
+            $username=$_POST['username'];
+            $email= $_POST['email'];
+            $password =$_POST['password'];
+            $id_role =$_POST['id_role'];
+
+            $register = new \App\Model\AuthentModel();
+            $register->setUsername($username);
+            $register->setEmail($email);
+            $register->setPassword($password);
+            $register->setIdRole($id_role);
+            if($register->register()){
+               
+                header('location:?uri=login');
+                echo 'success';
+                
+            }
+        }
+    }
 
     
 
