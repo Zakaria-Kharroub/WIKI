a46df7db0bca2ea607c616138e588d25ea02c49d
zakaria <zikokh012@gmail.com>
2024-01-11 03:06:39 +0100

    ajouter view author avec le AuthorController

---
+++ app/Controller/AuthorController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+
+class AuthorController{
+
+    public function index(){
+        include_once '../app/View/author.php';
+    }
+
+}
---
+++ app/View/author.php
@@ -0,0 +1,51 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <!-- head -->
+    <?php include '../app/View/layout/head.php' ?>
+<body class="bg-light">
+<!-- navbar -->
+<?php include '../app/View/layout/navbar.php' ?>
+
+
+
+<div class="mt-5"> 
+    <h1>hello autor</h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- bootstrap cdn -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
